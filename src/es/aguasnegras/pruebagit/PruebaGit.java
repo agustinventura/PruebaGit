90f7283534ea4eb832e4dd5282d1cb8be94d78c1
Agustín Ventura Carrasco <agustinventura@gmail.com>
2011-08-05 11:40:27 +0200

    Commit inicial del proyecto.

---
+++ src/es/aguasnegras/pruebagit/PruebaGit.java
@@ -0,0 +1,15 @@
+package es.aguasnegras.pruebagit;
+
+public class PruebaGit {
+
+	/**
+	 * Clase para ejemplo de uso de EGit.
+	 * 
+	 * @author Agustín Ventura Carrasco
+	 * @param args El programa no trata los argumentos.
+	 */
+	public static void main(String[] args) {
+		System.out.println("A ver si Git mola");
+	}
+
+}
